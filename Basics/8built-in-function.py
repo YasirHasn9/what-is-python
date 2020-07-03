6b2382a3557152d37fb2da0951145e457cac7e94
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-03 01:36:53 -0700

    built-in functions

---
+++ Basics/8built-in-function.py
@@ -0,0 +1,17 @@
+# Built-in function
+'''
+each language has its own built-in function depends on the amount of using them
+i have used the built-in function print() so many time
+so the people who work on python notice that they need of this kind of function 
+so the created it 
+
+in your joinery as programmer, you will need this kind of function even 
+you will use other code that been written by other programmers to make 
+so action and solve some problems for you 
+'''
+print(str(6))  # str : convert to string
+print(int(8.8))  # int :convert to integer number
+'''
+and there are so many built-in function which im not gonna go deep into them
+you can check them through the documentation of python , google it
+'''
