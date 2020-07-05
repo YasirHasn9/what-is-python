275b4ecce013b53375000a6ddf3e30c7acd82aa9
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-05 13:37:46 -0700

    parameters/arguments's function

---
+++ Basics/10function.py
@@ -0,0 +1,21 @@
+'''
+function can do magical things in programming, in order for them to do extraordinary 
+job you have to as a programmer provide the tools for them to do so 
+one of the tools that makes functions do this is adding parameters to them  inside the ()
+'''
+
+
+def sum_2_numbers(num1, num2):
+    print(num1 + num2)
+
+
+sum_2_numbers(44, 6)
+sum_2_numbers()  # ---> this isnt work because 2
+#  arguments been required to pass to the function
+'''
+now, you are asking with the hack is argument ?
+dont freak out with these terminologies , they just words and not a big deal 
+
+parameters are these been decleared within the function 
+argument are these been passed to the function when we call/invoke it
+'''
---
+++ Basics/9function.py
@@ -0,0 +1,55 @@
+# Function
+'''
+Almost every programming langauge has this feature and also each one of them 
+has its own way for writing function and python one of them
+what is function ?
+  -- it is a block of codes that could be 2 lines of code or 100 of lines,
+  that could do one purpose of many purposes depends on what do want out of this 
+  function 
+  persenely, i like singled-purpose function and also this is best practice
+
+  how do you write a function in python ?
+  1- def -- > declear a function by starting with the word def
+      def --> indicates that this is a function
+  
+  2- function's name so you can use where ever you want
+
+
+  3- () use brathensis () these can hold parameters 
+     we are gonna dig into them in the future but they are so easy :)
+
+  4- : colon to indicate the end of the declareation of the function 
+'''
+
+# declear the function named "get_x"
+
+
+def get_x():
+    # do something here
+    x = 100
+    print(x)
+
+
+# how to use this function in your programming ?
+#  easy just call/invoke the function , but how do you call it ?
+# like this
+get_x()  # and thats it
+'''
+notice how i wrote the function 
+there is 2 ways for writing function , camel case and snake case
+camel case --> myFunction
+                   or
+               myFunctionForDoingSomething
+    -- the second starts within uppercase letter 
+
+snake case my_function
+                   or
+           my_function_for_doing_something
+    -- there _ that separates the words 
+
+
+also the indentation is so important in writing functions when it comes to 
+python 
+whatever you wanna write within your function should be 4 spaces or at least 
+1 tap inside it 
+'''
