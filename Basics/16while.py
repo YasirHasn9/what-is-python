12d74fdda216148249e84877284f331100769af3
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-05 18:12:37 -0700

    while-loop

---
+++ Basics/16while.py
@@ -0,0 +1,19 @@
+# while-loop
+'''
+while loop is another way to iterate over items in the list 
+there is a slightly different between for-loop and while-loop ? 
+'''
+sum = 0
+num = 5
+i = 0
+while i < num:
+    sum += i
+    i += 1
+    print(sum)
+'''
+while loop requires some stuff to work with 
+initializer 
+so in while-loop is good for looping around something that we don't know how many times should we run
+the loop , it stops when the given condition return false.
+ so the while loop checks if the condition is true.
+'''
