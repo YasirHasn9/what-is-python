4e761d058c27bf1d3ca99ddddf3f8fa5a70955af
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-16 20:50:33 -0700

    doubly linked list , insert node after the node that we call

--- bigO/DoublyLinkedList.py
+++ bigO/DoublyLinkedList.py
@@ -7,107 +7,97 @@ so each node knows what are theses things
 
 # each node in the list should have value and 2 pointer one to prev and one to next
 
-class Node:
-    def __init__(self, data, prev=None, next=None):
-        self.data = data
-        self.prev = prev
-        self.next = next
+# class Node:
+#     def __init__(self, data, prev=None, next=None):
+#         self.data = data
+#         self.prev = prev
+#         self.next = next
 
 
-class DLL:
-    def __init__(self):
-        self.head = None
+# class DLL:
+#     def __init__(self):
+#         self.head = None
 
-    def append(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            new_node.prev = None
-            self.head = new_node
+#     def append(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             new_node.prev = None
+#             self.head = new_node
 
-        if self.head:
-            cur = self.head
-            while cur.next:
-                cur = cur.next
-            cur.next = new_node
-            new_node.prev = cur
-            new_node.next = None
+#         if self.head:
+#             cur = self.head
+#             while cur.next:
+#                 cur = cur.next
+#             cur.next = new_node
+#             new_node.prev = cur
+#             new_node.next = None
 
-    def prepend(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            new_node.prev = None
-            self.head = new_node
-        else:
-            self.head.prev = new_node
-            new_node.next = self.head
-            self.head = new_node
-            new_node.prev = None
+#     def prepend(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             new_node.prev = None
+#             self.head = new_node
+#         else:
+#             self.head.prev = new_node
+#             new_node.next = self.head
+#             self.head = new_node
+#             new_node.prev = None
 
-    def remove_head(self):
-        if self.head:
-            temp = self.head
-            cur = temp.next
-            temp.prev = None
-            self.head = cur
-            return temp.data
+#     def remove_head(self):
+#         if self.head:
+#             temp = self.head
+#             cur = temp.next
+#             temp.prev = None
+#             self.head = cur
+#             return temp.data
 
-    def delete(self, key):
-        cur = self.head
-        while cur:
+#     def delete(self, key):
+#         cur = self.head
+#         while cur:
 
-            # check if the key is the head
-            if cur.next == key and cur == self.head:
-                # the head is the only node in the list
-                if not cur.next:
-                    cur = None
-                    self.head = None
-                    return
+#             # check if the key is the head
+#             if cur.next == key and cur == self.head:
+#                 # the head is the only node in the list
+#                 if not cur.next:
+#                     cur = None
+#                     self.head = None
+#                     return
 
-    def print_l(self):
-        cur = self.head
-        while cur:
-            print("ggg", cur.data)
-            cur = cur.next
+#     def print_l(self):
+#         cur = self.head
+#         while cur:
+#             print("ggg", cur.data)
+#             cur = cur.next
 
 
-dll = DLL()
-dll.append(1)
-dll.delete(1)
-dll.print_l()
+# dll = DLL()
+# dll.append(1)
+# dll.delete(1)
+# dll.print_l()
 
-# def delete(self, key):
-#     cur = self.head
-#     while cur:
-#         # if the node that we want to delete heppens to be the first node(head) of list
-#         if cur.next == key and cur == self.head:
-#             # if the node is the only node in the list
-#             if not cur.next:
-#                 cur = None
-#                 self.head = None
-#                 return cur.data
-#             else:
-#                 # get the node after the cur
-#                 nxt = cur.next
-#                 # the pervious node of the next should none
-#                 nxt.prev = None
-#                 # make the deleted node none
-#                 cur = None
-#                 self.head = nxt
-#                 return
-#         elif cur.data == key:
-#             if cur.next:
-#                 nxt = cur.next
-#                 prev = cur.prev
-#                 prev.next = nxt
-#                 nxt.prev = prev
-#                 cur.next = None
-#                 cur.prev = None
-#                 cur = None
-#                 return
-#             else:
-#                 prev = cur.prev
-#                 prev.next = None
-#                 cur.prev = None
-#                 cur = None
-#                 return
-#         cur = cur.next
+
+# another way of writing solutions for doubly linked list
+
+'''
+each node holds a references to its next node and pervious node
+as well as it value
+'''
+
+
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    # [a] -> <- [b] -> <- [c] -> <- [d] -> <-none
+    # for instance, we want to add a new node after the b
+    # so the next for the new node should the next of the b which is c
+    # and the previous of c should the new node
+    # and the previous of the new node should the node that
+    # we want to insert after which is c
+    def insert_after(self, value):
+        cur_next = self.next
+        self.next = ListNode(value, self, cur_next)
+        if cur_next:
+            cur_next.prev = self.next
--- bigO/binarySearch.py
+++ bigO/binarySearch.py
@@ -1,9 +1,37 @@
-# Binary Search
-
 '''
-Binary search a tree of data structure in which each node has at most
+
+You can think of tree as linked list without of the constrains of having each node
+points to another node where in trees each node could points to 2 nodes at most
+                     * root 
+                    / \
+                   *   * parent
+                      / \
+                    *    * children 
+                  /  \  
+                 *    * leaf
+
+
+[root] is the top most node.
+[child] is the node that directly connected to other node when moving away from 
+the root node.
+[parent] is the node that directely connected to another node when moving towards 
+the parent
+[siblings ] nodes that share the same parent
+[leaf] node that has no children
+
+
+
+Binary search for any given node , all the values on the left subtree are less than 
+the given node and the node on the right are bigger that the given node
 2 children left node and right node
 left node is smaller than the parent and the right one is greater than the parent
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.value = Value
+        self.left = left_subtree
+        self.right = right_subtree
 
 
 to say this tree is full then the nodes should have 2 child
@@ -53,20 +81,3 @@ post travesal      L R N left right and node
 and all of them we categorized them under the concept of 
        Depth_first traversal
 '''
-
-
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-
-class BinarySearch(object):
-    def __init__(self, root):
-        self.root = Node(root)
-
-
-tree = BinarySearch(1)
-tree.root.left(2)
-tree.root.right(3)
