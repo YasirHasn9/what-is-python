3dcf041eb5857bb8210fab8813bedec12883662c
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-18 14:59:31 -0700

    implementaion of the selection sort

--- search/sorting.py
+++ search/sorting.py
@@ -25,8 +25,48 @@ def bubble_sort(arr):
                 swapped = True
 
 
+# worst case , the time complexity is O(n^2)
 random_list_of_nums = [5, 2, 1, 8, 4]
+print("bubble sort sort before sorting \n", random_list_of_nums,  "\n")
 bubble_sort(random_list_of_nums)
-print(random_list_of_nums)
+print("after \n" ,random_list_of_nums)
+
+
+# Selection sorting
+'''
+Selecting sorting is another way for inserting elements in the list to sort them
+imagine we have a dock of cards and we want to sort them 
+1. check every card in the list
+2. select the smallest card in the list
+3. swap what the ele in the index 0, now the ele is in the right spot
+4. repeat the process
+
+so the element in the list would divide into 2 parts sortted one and unsorted.
+treated the leftmost part as sorted and jump to the second part to find the smallest item in it 
+and compare it to the sorted item in the leftmost one
+'''
+
+
+def selection_sort(arr):
+    # i corresponds to number of items in list
+    for i in range(len(arr)):
+        # consider the first index 0 in arr is the lowest value
+        lowest_value_index = i
+        # loop over the rest of the items in the arr
+        for j in range(i + 1, len(arr)):
+            # find if there is item that is lowest than the sorted item
+            if arr[j] < arr[lowest_value_index]:
+                lowest_value_index = j
+
+            # make the swap with lowest unsorted item with the first unsorted element
+            arr[i], arr[lowest_value_index] = arr[lowest_value_index], arr[i]
+
+# the time complexity is O(n^2)
+random_list_of_nums = [12, 8, 3, 20, 11]
+print("###################")
+print("selection sort sort before sorting \n", random_list_of_nums, "\n")
+selection_sort(random_list_of_nums)
+print("after \n",random_list_of_nums)
+
 
 
